9f0274ef0c38dc806c28f7d8bbedbd96d1bfaed0
aGhouzlane <ahmedghouzlane@gmail.com>
2021-06-11 15:24:09 -0400

    Add dbcontext class

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+  public class AnimalShelterContext : DbContext
+  {
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options) : base(options)
+    {
+    }
+    public DbSet<Animal> Animals { get; set; }
+  }
+}
