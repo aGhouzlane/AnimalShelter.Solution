8394fc5432cd0c2cb4d11b47b5d14de66007af6b
aGhouzlane <ahmedghouzlane@gmail.com>
2021-06-21 10:38:07 -0400

    Change SeedData sample database data

--- AnimalShelter/Migrations/20210611220642_SeedData.cs
+++ AnimalShelter/Migrations/20210611220642_SeedData.cs
@@ -2,49 +2,49 @@
 
 namespace AnimalShelter.Migrations
 {
-    public partial class SeedData : Migration
+  public partial class SeedData : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.InsertData(
-                table: "Animals",
-                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
-                values: new object[,]
-                {
-                    { 1, 7, "Female", "Matilda", "Woolly Mammoth" },
-                    { 2, 10, "Female", "Rexie", "Dinosaur" },
-                    { 3, 2, "Female", "Matilda", "Dinosaur" },
-                    { 4, 4, "Male", "Pip", "Shark" },
-                    { 5, 22, "Male", "Bartholomew", "Dinosaur" }
-                });
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DeleteData(
-                table: "Animals",
-                keyColumn: "AnimalId",
-                keyValue: 1);
-
-            migrationBuilder.DeleteData(
-                table: "Animals",
-                keyColumn: "AnimalId",
-                keyValue: 2);
-
-            migrationBuilder.DeleteData(
-                table: "Animals",
-                keyColumn: "AnimalId",
-                keyValue: 3);
-
-            migrationBuilder.DeleteData(
-                table: "Animals",
-                keyColumn: "AnimalId",
-                keyValue: 4);
-
-            migrationBuilder.DeleteData(
-                table: "Animals",
-                keyColumn: "AnimalId",
-                keyValue: 5);
-        }
+      migrationBuilder.InsertData(
+          table: "Animals",
+          columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+          values: new object[,]
+          {
+                    { 1, 45, "Male", "Mimi", "Cat" },
+                    { 2, 14, "Female", "Snowy", "Cat" },
+                    { 3, 20, "Female", "Rocky", "Dog" },
+                    { 4, 45, "Female", "Sunny", "Bird" },
+                    { 5, 23, "Male", "Pikachu", "Bird" }
+          });
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DeleteData(
+          table: "Animals",
+          keyColumn: "AnimalId",
+          keyValue: 1);
+
+      migrationBuilder.DeleteData(
+          table: "Animals",
+          keyColumn: "AnimalId",
+          keyValue: 2);
+
+      migrationBuilder.DeleteData(
+          table: "Animals",
+          keyColumn: "AnimalId",
+          keyValue: 3);
+
+      migrationBuilder.DeleteData(
+          table: "Animals",
+          keyColumn: "AnimalId",
+          keyValue: 4);
+
+      migrationBuilder.DeleteData(
+          table: "Animals",
+          keyColumn: "AnimalId",
+          keyValue: 5);
+    }
+  }
 }
